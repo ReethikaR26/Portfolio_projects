19d1a22cf7219f90b2723a5639df4461ec8d136d
ReethikaR26 <reethikame26r@gmail.com>
2026-04-08 23:48:44 +1000

    Update ImageGenerator.jsx

--- ai-image-generator/src/Components/ImageGenerator/ImageGenerator.jsx
+++ ai-image-generator/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -3,7 +3,7 @@ import default_image from '../Assets/default_image.png'
 import './ImageGenerator.css'
 const ImageGenerator = () => {
 
-    const [ImageUrl,setImageUrl] = useState("/") ;
+    const [history,setHistory] = useState([]);
     let InputRef=useRef(null);
     const [loading,setLoading] = useState(false);
 
@@ -12,28 +12,32 @@ const ImageGenerator = () => {
             return 0;
         }
         setLoading(true);
-        const response = await fetch(
-            "https://api.openai.com/v1/images/generations",
-            {
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json",
-                    Authorization:
-                    "Bearer ",
-                    "User-Agent":"Chrome",
-                },
-                body: JSON.stringify({
-                    model: "gpt-image-1",
-                    prompt: InputRef.current.value,
-                    n:1,
-                    size:"auto",
-                }),
-            }
-        );
-        let data=await response.json();
-        console.log(data);
-        let data_array = data.data;
-        setImageUrl(data_array[0].url);
+        try{const response = await fetch(
+                "http://localhost:5000/api/generate",
+                {
+                    method: "POST",
+                    headers:{
+                        "Content-Type": "application/json"},    
+                    body: JSON.stringify({
+                        prompt: InputRef.current.value,
+                    }),
+                });
+                if (!response.ok) {
+            const errData = await response.json();
+            throw new Error(errData.error || "Failed to generate");
+        }
+
+            const imageBlob = await response.blob();
+
+            const imageUrl = URL.createObjectURL(imageBlob);
+            
+            
+            setHistory((prevHistory)=> [imageUrl, ...prevHistory].slice(0,4));
+            
+        }catch(error){
+            console.error(error);
+            alert("Failed to generate image");
+        }
         setLoading(false);
     }
 
@@ -42,13 +46,28 @@ const ImageGenerator = () => {
             <div className="header">AI Image <span>Generator</span></div>
             <div className="img-loading">
                 <div className="image">
-                    <img src={ImageUrl==='/'?default_image:ImageUrl} alt="Default image" />
+                    <img src={history.length > 0 ? history[0] : default_image} alt="Generated" />
                 </div>
                 <div className="loading">
                     <div className={loading?"loading-bar-full":"loading-bar"}></div>
                     <div className={loading?"loading-text":"display-none"}>Loading...</div>
                 </div>
             </div>
+            { history.length > 1 && (
+                <div className="gallery">
+                    <h3>Your history</h3>
+                    <div className="gallery-items">
+                        {history.map((img,index) => (
+                            <img
+                             key={index} src={img}
+                             alt={`Recent ${index}`}
+                             className={index===0 ? "active-thumb" : "thumb"}
+                            />
+                        ))}
+                    </div>
+                </div>
+            )
+            }
             <div className="search">
                 <input type="text" ref={InputRef} className="search-input" placeholder='Describe the image you want to generate..'/>
                 <div className="search-btn" onClick={() => {ImageGenerator()}}>Generate</div>
